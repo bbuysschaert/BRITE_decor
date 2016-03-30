f94e15b99b3466728f93b7e88d17f75db43f588e
bbuysschaert <bram.buysschaert@gmail.com>
2016-03-30 11:15:58 +0200

    Update read_PSFcorrection_save_example.py

--- read_PSFcorrection_save_example.py
+++ read_PSFcorrection_save_example.py
@@ -22,7 +22,7 @@ Last update 21 March 2016
 #===============================================================================
 # 				Packages
 #===============================================================================
-import CheckMatplotlib # LOCAL ROUTINE, remove
+#import CheckMatplotlib # LOCAL ROUTINE, remove
 import numpy as np
 
 import scipy.interpolate as scInterp
